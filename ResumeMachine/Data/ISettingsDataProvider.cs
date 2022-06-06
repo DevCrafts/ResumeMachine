709a957543fdfc5f63e7c012a76fce8dd417a5b3
vadimffe <vadim.ffe@gmail.com>
2022-06-07 02:12:01 +0300

    Layout fixes

---
+++ ResumeMachine/Data/ISettingsDataProvider.cs
@@ -0,0 +1,12 @@
+﻿using ResumeMachine.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ResumeMachine.Data
+{
+  public interface ISettingsDataProvider
+  {
+    Task<List<Setting>> LoadSettingsFromJsonAsync();
+    Task SaveSettingsToJsonAsync(List<Setting> settings);
+  }
+}
--- ResumeMachine/Data/SettingsDataProvider.cs
+++ ResumeMachine/Data/SettingsDataProvider.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace ResumeMachine.Data
 {
-  public class SettingsDataProvider
+  public class SettingsDataProvider : ISettingsDataProvider
   {
     public async Task<List<Setting>> LoadSettingsFromJsonAsync()
     {
